9c2842cd469c7dede705654c89e571f50b47f6cf
Dev Vardhan Singh Bhadauria <devvardhan456@gmail.com>
2024-10-01 23:59:01 +0530

    image create function added

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -15,8 +15,27 @@ const CreatePost = () => {
     })
     const [Loading, setLoading] = useState(false)
 
-    const generateImage = () => {
-
+    const generateImage = async () => {
+        if (form.prompt) {
+            try {
+                setgeneratingImg(true)
+                const response = await fetch('http://localhost:5050/api/v1/dalle', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({ prompt: form.prompt })
+                })
+                const data = await response.json
+                setform({ ...form, photo: `data:image/jpeg;base64,${data.photo}` })
+            } catch (error) {
+                alert(error)
+            } finally {
+                setgeneratingImg(false)
+            }
+        } else {
+            alert('Enter a valid prompt')
+        }
     }
 
     const handleSubmit = () => {
--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -1,4 +1,4 @@
-import express from 'express'
+import express, { response } from 'express'
 import * as dotenv from 'dotenv'
 import OpenAI from 'openai';
 
@@ -7,11 +7,32 @@ dotenv.config()
 const router = express.Router()
 
 const openai = new OpenAI({
-    apiKey: process.env.OPENAI_API_KEY
+    apiKey: process.env.ALTERNATE_OPENAI_API_KEY
 });
+
 
 router.route('/').get((req, res) => {
     res.send("hello from dalle")
+})
+
+
+router.route('/').post(async (req, res) => {
+    try {
+        const { prompt } = req.body;
+
+        const aiResponse = await openai.images.generate({
+            prompt,
+            n: 1,
+            size: '1024x1024',
+            response_format: 'b64_json',
+        });
+
+        const image = aiResponse.data.data[0].b64_json;
+        res.status(200).json({ photo: image });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send(error?.response.data.error.message || 'Something went wrong');
+    }
 })
 
 export default router
